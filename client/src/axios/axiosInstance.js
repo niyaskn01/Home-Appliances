fbcd1b6e8a141503ffdf9958eaba5d0b2f02a1b4
niyas <niyaskn01@gmail.com>
2024-02-08 11:23:10 +0530

    api changed

--- client/src/axios/axiosInstance.js
+++ client/src/axios/axiosInstance.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const instance=axios.create({
-  baseURL:'https://home-appliances-xi.vercel.app'
+  baseURL:'https://home-appliances-two.vercel.app'
 })
 
-export const baseURL='https://home-appliances-xi.vercel.app'
+export const baseURL='https://home-appliances-two.vercel.app'
 
 export default instance
