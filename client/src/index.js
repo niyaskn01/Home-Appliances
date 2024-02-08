f9fa4d3d26bc9c46aaf15b7cac900c9c00124364
niyas <niyaskn01@gmail.com>
2024-02-08 12:37:50 +0530

    small change

--- client/src/index.js
+++ client/src/index.js
@@ -9,11 +9,11 @@ import store from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <Provider store={store}>
-      <BrowserRouter>
         <App />
-      </BrowserRouter>
     </Provider>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
